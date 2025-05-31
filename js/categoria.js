16c1aca590edc62b7ee0d132d6d362c570dbb984
Planverde <planverde@example.com>
2025-05-31 12:55:05 -0300

    ultimo-commit

--- js/categoria.js
+++ js/categoria.js
@@ -20,90 +20,90 @@ const productosPorCategoria = {
         { nombre: 'Pierna Deshuesada Bbq', precio: 25, imagen: '/images/pollos-a-la-parrilla/pierna-deshuesada-bbq.jpg' }
     ],
     'POLLO A LA LEÑA': [
-        { nombre: 'Combo Familiar #1', precio: 70, imagen: '/images/Pollo-a-la-leña/pollo-familiar-combo-1.jpg' },
-        { nombre: 'Combo Familiar #2', precio: 70, imagen: '/images/Pollo-a-la-leña/pollo-familiar-combo-2.jpg' },
-        { nombre: 'Combo Familiar #3', precio: 73, imagen: '/images/Pollo-a-la-leña/pollo-familiar-combo-3.jpg' },
-        { nombre: 'Combo Familiar #4', precio: 88, imagen: '/images/Pollo-a-la-leña/pollo-familiar-combo-4.jpg' },
-        { nombre: '1 Pollo a La Leña', precio: 60, imagen: '/images/Pollo-a-la-leña/1-pollo-a-la-leña.jpg' },
+        { nombre: 'Combo Familiar #1', precio: 73, imagen: '/images/Pollo-a-la-leña/pollo-familiar-combo-1.jpg' },
+        { nombre: 'Combo Familiar #2', precio: 78, imagen: '/images/Pollo-a-la-leña/pollo-familiar-combo-2.jpg' },
+        { nombre: 'Combo Familiar #3', precio: 80, imagen: '/images/Pollo-a-la-leña/pollo-familiar-combo-3.jpg' },
+        { nombre: 'Combo Familiar #4', precio: 85, imagen: '/images/Pollo-a-la-leña/pollo-familiar-combo-4.jpg' },
+        { nombre: '1 Pollo a La Leña', precio: 68, imagen: '/images/Pollo-a-la-leña/1-pollo-a-la-leña.jpg' },
         { nombre: '1/2 Pollo a La Leña', precio: 37, imagen: '/images/Pollo-a-la-leña/medio-pollo-a-la-parrilla.jpg' },
         { nombre: '1/4 Pollo a La Leña', precio: 20, imagen: '/images/Pollo-a-la-leña/cuarto-pollo-a-la-leña.jpg' }
     ],
     'PIQUEOS': [
         { nombre: 'Alitas a La BBQ- 14 Unidades', precio: 32, imagen: '/images/Piqueos/alitas-a-la-bbq.jpg' },
         { nombre: 'Alitas A la BBQ Picantes- 14 Unidades', precio: 32, imagen: '/images/Piqueos/alitas-a-la-bbq.jpg' },
-        { nombre: 'Anticucho Parrillero', precio: 23, imagen: '/images/Piqueos/anticucho-parrillero.jpg' },
+        { nombre: 'Anticucho Parrillero', precio: 25, imagen: '/images/Piqueos/anticucho-parrillero.jpg' },
         { nombre: 'Chicharron de Pollo', precio: 25, imagen: '/images/Piqueos/chicharron-de-pollo.jpg' },
         { nombre: 'Chorizo', precio: 20, imagen: '/images/Piqueos/chorizos.jpg' },
-        { nombre: 'Mollejitas De Pollo', precio: 18, imagen: '/images/Piqueos/mollejitas-de-pollo.jpg' },
+        { nombre: 'Mollejitas De Pollo', precio: 20, imagen: '/images/Piqueos/mollejitas-de-pollo.jpg' },
         { nombre: 'Tequeños Leña Y Sabor', precio: 15, imagen: '/images/Piqueos/Tequeños-leña-y-sabor.jpg' },
         { nombre: 'Tequeños Tradicionales', precio: 15, imagen: '/images/Piqueos/Tequeños-leña-y-sabor.jpg' },
         
     ],
     'NUESTRAS PASTAS': [
         { nombre: 'Tallarines Verdes Con Bife', precio: 42, imagen: '/images/pastas/tallarin-verde-con-bife.jpg' },
-        { nombre: 'Tallarin Verde Con Chuleta', precio: 29, imagen: '/images/pastas/tallarin-verde-con-chuleta.jpg' },
-        { nombre: 'Tallarin Verde Con Churrasco', precio: 35, imagen: '/images/pastas/tallarin-verde-con-churrasco.jpg' },
-        { nombre: 'Tallarin Verde Con Pechuga a la Parrilla', precio: 32, imagen: '/images/pastas/tallarines-verdes-con-pechuga-a-la-parrilla.jpg' },
+        { nombre: 'Tallarin Verde Con Chuleta', precio: 32, imagen: '/images/pastas/tallarin-verde-con-chuleta.jpg' },
+        { nombre: 'Tallarin Verde Con Churrasco', precio: 40, imagen: '/images/pastas/tallarin-verde-con-churrasco.jpg' },
+        { nombre: 'Tallarin Verde Con Pechuga a la Parrilla', precio: 35, imagen: '/images/pastas/tallarines-verdes-con-pechuga-a-la-parrilla.jpg' },
         { nombre: 'Tallarines A la Huancaina Con Bife', precio: 36, imagen: '/images/pastas/tallarines-huancaina-con-bife.jpg' },
         { nombre: 'Tallarines A la Huancaina Con Chuleta', precio: 32, imagen: '/images/pastas/tallarines-huaincana-chuleta.jpg' },
         { nombre: 'Tallarines a la Huancaina Con Churrasco', precio: 37, imagen: '/images/Pastas/tallarines-huancaina-con-churrasco.jpg' },
-        { nombre: 'Tallarines a La Huancaina con Lomo Saltado', precio: 37, imagen: '/images/Pastas/tallarines-huaicana-lomo.jpg' },
+        { nombre: 'Tallarines a La Huancaina con Lomo Saltado', precio: 45, imagen: '/images/Pastas/tallarines-huaicana-lomo.jpg' },
         { nombre: 'Tallarines A La Huancaina Con Pechuga A La Parrilla', precio: 35, imagen: '/images/Pastas/tallarines-huncaina-pechuga.jpg' },
         { nombre: 'Tallarines A Lo Alfredo', precio: 25, imagen: '/images/Pastas/tallarines-a-lo-alfredo.jpg' },
         { nombre: 'Tallarines A Lo Alfredo Con Bife', precio: 40, imagen: '/images/Pastas/tallarines-huancaina-con-bife.jpg' },
         { nombre: 'Tallarines A Lo Alfredo Con Pechuga', precio: 35, imagen: '/images/Pastas/tallarines-huncaina-pechuga.jpg' }
     ],
     'NUESTRAS PARRILLAS': [
-        { nombre: 'Parrilla Familiar', precio: 110, imagen: '/images/Nuestras-Parrillas/Parrilla-Familiar.jpg' },
-        { nombre: 'Parrilla Leña Y Sabor', precio: 139, imagen: '/images/Nuestras-Parrillas/Parrilla-leña-y-sabor.jpg' },
-        { nombre: 'Parrilla Mediana', precio: 80, imagen: '/images/Nuestras-Parrillas/Parrilla-Familiar.jpg' },
-        { nombre: 'Parrilla Para Dos', precio: 60, imagen: '/images/Nuestras-Parrillas/Parrilla-para-dos.jpg' }
+        { nombre: 'Parrilla Familiar', precio: 150, imagen: '/images/Nuestras-Parrillas/Parrilla-Familiar.jpg' },
+        { nombre: 'Parrilla Leña Y Sabor', precio: 130, imagen: '/images/Nuestras-Parrillas/Parrilla-leña-y-sabor.jpg' },
+        { nombre: 'Parrilla Mediana', precio: 70, imagen: '/images/Nuestras-Parrillas/Parrilla-Familiar.jpg' },
+        { nombre: 'Parrilla Para Dos', precio: 90, imagen: '/images/Nuestras-Parrillas/Parrilla-para-dos.jpg' }
     ],
     'NUESTRAS CARNES': [
-        { nombre: 'Lomo Fino', precio: 55, imagen: 'images/lomo-fino.jpg' },
-        { nombre: 'Bife de Chorizo', precio: 60, imagen: 'images/bife-chorizo.jpg' },
-        { nombre: 'Picaña', precio: 58, imagen: 'images/picaña.jpg' },
-        { nombre: 'Asado de Tira', precio: 62, imagen: 'images/asado-tira.jpg' },
-        { nombre: 'Entraña', precio: 65, imagen: 'images/entraña.jpg' }
+        { nombre: 'Lomo Fino', precio: 55, imagen: '/images/Nuestras-Carnes/lomo_fino.jpg' },
+        { nombre: 'Bife Angosto', precio: 40, imagen: '/images/Nuestras-Carnes/bife.jpg' },
+        { nombre: 'Chuleta A La Parrilla', precio: 28, imagen: '/images/Nuestras-Carnes/chuleta_parrilla.jpg' },
+        { nombre: 'Churrasco A La Parrilla', precio: 35, imagen: '/images/Nuestras-Carnes/churrasco-parrilla.jpg' },
+        { nombre: 'Costilla A La BBQ', precio: 45, imagen: '/images/Nuestras-Carnes/Costilla-BBQ.jpg' }
     ],
     'LOS CRIOLLAZOS': [
-        { nombre: 'Lomo Saltado', precio: 38, imagen: 'images/lomo-saltado.jpg' },
-        { nombre: 'Ají de Gallina', precio: 32, imagen: 'images/aji-gallina.jpg' },
-        { nombre: 'Tacu Tacu con Lomo', precio: 40, imagen: 'images/tacu-tacu-lomo.jpg' },
-        { nombre: 'Seco de Res', precio: 36, imagen: 'images/seco-res.jpg' },
-        { nombre: 'Arroz con Pollo', precio: 30, imagen: 'images/arroz-pollo.jpg' },
-        { nombre: 'Cau Cau', precio: 28, imagen: 'images/cau-cau.jpg' },
-        { nombre: 'Carapulcra', precio: 29, imagen: 'images/carapulcra.jpg' },
-        { nombre: 'Anticuchos', precio: 27, imagen: 'images/anticuchos.jpg' },
-        { nombre: 'Papa a la Huancaína', precio: 18, imagen: 'images/papa-huancaina.jpg' },
-        { nombre: 'Ceviche Criollo', precio: 35, imagen: 'images/ceviche-criollo.jpg' },
-        { nombre: 'Chanfainita', precio: 26, imagen: 'images/chanfainita.jpg' }
+        { nombre: 'Bistec A Lo Pobre', precio: 30, imagen: '/images/Criollazos/bistec_pobre.jpg' },
+        { nombre: 'Chaufa De Cecina', precio: 32, imagen: '/images/Criollazos/Chaufa_cezina.jpg' },
+        { nombre: 'Chaufa Leña Y Sabor', precio: 25, imagen: '/images/Criollazos/chaufa_leñaysabor.jpg' },
+        { nombre: 'Lomo A Lo Pobre', precio: 42, imagen: '/images/Criollazos/lomo_pobre.jpg' },
+        { nombre: 'Lomo Saltado', precio: 35, imagen: '/images/Criollazos/lomo_saltado_chaufa.jpg' },
+        { nombre: 'Lomo Saltado Con Chaufa', precio: 40, imagen: '/images/Criollazos/lomo_saltado_chaufa.jpg' },
+        { nombre: 'Mostrito Leña Y Sabor', precio: 27, imagen: '/images/Criollazos/mostrito.jpg' },
+        { nombre: 'Pollo Saltado', precio: 25, imagen: '/images/Criollazos/pollo_saltado.jpg' },
+        { nombre: 'Pollo Saltado A Lo Pobre', precio: 30, imagen: '/images/Criollazos/pollo_saltado_pobre.jpg' },
+        { nombre: 'Tallarin Saltado De Carne', precio: 33, imagen: '/images/Criollazos/tallarin_saltado_carne.jpg' },
+        { nombre: 'Tallarin Saltado De Pollo', precio: 27, imagen: '/images/Criollazos/tallarin_saltado_pollo.jpg' }
     ],
     'ENSALADAS': [
-        { nombre: 'Ensalada César', precio: 28, imagen: 'images/ensalada-cesar.jpg' },
-        { nombre: 'Ensalada Mixta', precio: 25, imagen: 'images/ensalada-mixta.jpg' },
-        { nombre: 'Ensalada de Quinoa', precio: 30, imagen: 'images/ensalada-quinoa.jpg' },
-        { nombre: 'Ensalada de Pollo', precio: 27, imagen: 'images/ensalada-pollo.jpg' }
+        { nombre: 'Ensalada Clasica Grande', precio: 10, imagen: '/Fotos/Ensalada.jpg' },
+        { nombre: 'Ensalada Con Pechuga A La Parrilla', precio: 28, imagen: '/images/ensaladas/ensalada_pechuga.jpg' },
+        { nombre: 'Ensalada Con Pierna Deshuesada A La Parrilla', precio: 27, imagen: '/images/ensaladas/ensalada_muslo.jpg' },
+        { nombre: 'Ensalada de Palta', precio: 15, imagen: '/images/ensaladas/ensalada_palta.png' }
     ],
     'BEBIDAS': [
-        { nombre: 'Gaseosa Personal', precio: 6, imagen: 'images/gaseosa.jpg' },
-        { nombre: 'Gaseosa 1L', precio: 12, imagen: 'images/gaseosa-1l.jpg' },
-        { nombre: 'Agua Mineral', precio: 5, imagen: 'images/agua-mineral.jpg' },
-        { nombre: 'Chicha Morada', precio: 8, imagen: 'images/chicha-morada.jpg' },
-        { nombre: 'Limonada', precio: 9, imagen: 'images/limonada.jpg' },
-        { nombre: 'Cerveza Personal', precio: 10, imagen: 'images/cerveza-personal.jpg' },
-        { nombre: 'Cerveza 1L', precio: 18, imagen: 'images/cerveza-1l.jpg' },
-        { nombre: 'Jugo de Naranja', precio: 11, imagen: 'images/jugo-naranja.jpg' },
-        { nombre: 'Agua con Gas', precio: 7, imagen: 'images/agua-gas.jpg' },
-        { nombre: 'Bebida Energética', precio: 14, imagen: 'images/bebida-energetica.jpg' }
+        { nombre: 'Agua Mineral', precio: 4, imagen: '/images/bebidas/AguaCielo.webp' },
+        { nombre: 'Chicha Morada 1L', precio: 18, imagen: '/images/bebidas/chicha_morada.jpeg' },
+        { nombre: 'Coca Cola 1,5 Lts', precio: 10, imagen: '/images/bebidas/coca_litroymedio.jpg' },
+        { nombre: 'Coca Cola 600 ', precio: 6, imagen: '/images/bebidas/cocapersonal.png' },
+        { nombre: 'Inka Kola 1,5 Lts', precio: 10, imagen: '/images/bebidas/bebidas-inca-kola-15-web.png' },
+        { nombre: 'Inka Kola 500', precio: 6, imagen: '/images/bebidas/bebidas-inca-kola-500-web.png' },
+        { nombre: 'Limonada Americana 1 Lt', precio: 16, imagen: '/images/bebidas/limonada_americana.webp' },
+        { nombre: 'Limonada De Hierba Luisa 1 Lt', precio: 18, imagen: '/images/bebidas/limonada_hierba_luisa.jpeg' },
+        { nombre: 'Maracuya 1 Lt', precio: 18, imagen: '/images/bebidas/maracuya_1lt.jpeg' },
+        { nombre: 'Maracuya Con Limon 1 Lt', precio: 20, imagen: '/images/bebidas/maracuya_conlimon.webp' }
     ],
     'ADICIONALES': [
-        { nombre: 'Papas Fritas', precio: 10, imagen: 'images/papas-fritas.jpg' },
-        { nombre: 'Arroz Blanco', precio: 8, imagen: 'images/arroz-blanco.jpg' },
-        { nombre: 'Ensalada Simple', precio: 7, imagen: 'images/ensalada-simple.jpg' },
-        { nombre: 'Salsas Variadas', precio: 5, imagen: 'images/salsas-variadas.jpg' },
-        { nombre: 'Pan al Ajo', precio: 6, imagen: 'images/pan-ajo.jpg' },
-        { nombre: 'Yuca Frita', precio: 9, imagen: 'images/yuca-frita.jpg' }
+        { nombre: 'Papas Fritas', precio: 15, imagen: '/images/adicionales/papas_fritas.jpg' },
+        { nombre: 'Chaufa', precio: 13, imagen: '/images/adicionales/chaufa.jpg' },
+        { nombre: 'Huevos Fritos', precio: 4, imagen: '/images/adicionales/huevos.jpeg' },
+        { nombre: 'Platano Frito', precio: 4, imagen: '/images/adicionales/platanos.jpg' },
+        { nombre: 'Porcion De Arroz', precio: 4, imagen: '/images/adicionales/porcion_arroz.jpg' },
+        { nombre: 'Unidad De Chorizo', precio: 6, imagen: '/images/Piqueos/chorizos.jpg' }
     ]
 };
 
